6c1f5e641e2aabdc017690ab25bbbb96425ad180
truemanz <ziyaulhaqkhan60@gmail.com>
2022-09-17 03:32:45 +0530

    Python Addition Program

---
+++ Pyadd.py
@@ -0,0 +1,11 @@
+number1 = int(input("Enter 1st Number: "))
+
+print("\nEntered number is" , number1)
+
+number2 = int(input("\nEnter 2nd Number: "))
+
+print("\n2nd Entered number is" , number2)
+
+total = number1 + number2
+
+print("\nAddition of", number1, "&", number2, "is", total) 
